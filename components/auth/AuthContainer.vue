ba64db9d8f022cca80577589a0c80d180ace9abb
mahdij <mahdimn2011@yahoo.com>
2022-01-12 12:55:41 +0330

    Auth Container(wrapper) added

---
+++ components/auth/AuthContainer.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="w-full h-full flex justify-center items-center p-8 mt-16">
+		<img src="~/assets/img/light.png" class="mr-6" />
+		<slot></slot>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+	head(){
+		return {
+			title: 'auth'
+		}
+	}
+})
+</script>
